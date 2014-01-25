6c5407fad36a490a4b4e783227736eeb4498cac7
liuweilong <viclong1993@gmail.com>
2014-01-26 00:30:46 +0800

    FirstUpload

---
+++ UniCalendar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UniCalendar
+//
+//  Created by Liu Weilong on 24/1/14.
+//  Copyright (c) 2014 Liu Weilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UniCalendar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UniCalendar
+//
+//  Created by Liu Weilong on 24/1/14.
+//  Copyright (c) 2014 Liu Weilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
